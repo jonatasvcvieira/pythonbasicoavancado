864870afb461a391ca8c4beb7abf5be0dc57ebce
jvasconcelosdev <jonatasvieir@gmail.com>
2021-08-09 09:22:59 -0300

    Funções

---
+++ aula13/aula13.py
@@ -0,0 +1,53 @@
+import re
+
+def is_float(val):
+    if isinstance(val, float): return True
+    if re.search(r'^\-{,1}[0-9]+\.{1}[0-9]+$', val): return True
+
+    return False
+
+def is_int(val):
+    if isinstance(val, int): return True
+    if re.search(r'^\-{,1}[0-9]+$', val): return True
+
+    return False
+
+def is_number(val):
+    return is_int(val) or is_float(val)
+
+
+num1 = input('Digite um número:')
+num2 = input('Digite um número:')
+
+if is_number(num1) and is_number(num2):
+    num1 = float(num1)
+    num2 = float(num2)
+
+    print(num1 + num2)
+else:
+    print('ERROR')
+
+'''
+try: 
+    num1 = float(num1)
+    num2 = float(num2)
+    
+    print(num1 + num2)
+except:
+    print('ERROR')
+'''
+'''
+###########
+#  USAGE  #
+###########
+
+# Float
+print(is_float('-101.0112'))  # True
+# Int
+print(is_int('-1010112'))  # True
+# Numbers in general (float ou int)
+print(is_number('-1010.112'))  # True
+
+# False
+print(is_number('123a'))  # False
+'''
